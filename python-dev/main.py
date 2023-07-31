82a4a343bd5a0e2abe9446b2d89ac8d3c9b48f19
Talita Dias <123988772+talitasdias@users.noreply.github.com>
2023-07-30 20:21:04 -0400

    Finalizando o chat

--- python-dev/main.py
+++ python-dev/main.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+#criar a funcionalidada de enviar mensagem
+@socketio.on("message")
+def gerenciar_msg(mensagem):
+    send(mensagem, broadcast=True)
 
 # Criar a 1ª página = 1ª rota (caminho) = "/"
 @app.route("/") #decorator -> atribui uma função para o que for especificado abaixo dele
@@ -9,4 +16,4 @@ def homepage():
 
 
 # Rodar o app
-app.run()
+socketio.run(app, host="192.168.1.6")
